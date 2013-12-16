40ba9ab2757e3c0e95f63b4a1c5dab369ae218bd
Christophe Gragnic <christophegragnic@gmail.com>
2013-12-16 21:23:05 +0100

    Add db script.

--- Tests/HelloWorldTest.php
+++ Tests/HelloWorldTest.php
@@ -12,6 +12,30 @@ class HelloWorldTest extends PHPUnit_Framework_TestCase
         $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->pdo->query("CREATE TABLE hello (what VARCHAR(50) NOT NULL)");
+        $this->pdo->query("CREATE TABLE `personnages`
+(`id` 			smallint(5) 	not null 	auto_increment,
+ `nom` 			varchar(50) 	COLLATE latin1_general_ci	not null,
+ `puissance` 	int,
+ `degats` 		int,
+ `experience` 	int,
+ `niveau` 		int,
+ PRIMARY KEY    (`id`)
+ );");
+
+        $this->pdo->query("INSERT into personnages ( `nom`, `puissance`, `degats`, `experience`, `niveau`)
+VALUES
+ ('Roi', 		80,0,50, 13),
+ ('Reine', 		50,0,50, 12),
+ ('Fou', 		20,0,20, 11),
+ ('Tour', 		20,0,50, 10),
+ ('Combattant', 80,0,50, 9),
+ ('Soldat', 	50,0,20, 8),
+ ('Garde', 		50,0,20, 7),
+ ('Fantassin', 	50,0,20, 6),
+ ('Alerte', 	20,0,50, 5),
+ ('Déesse', 	20,0,80,4),
+ ('Amazone', 	20,0,50, 3)
+;");
     }
 
     public function tearDown()
